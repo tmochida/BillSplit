f4fbb70ea3f5ce60193db581de087075f8a6296a
Takeshi Mochida <takeshimochida@gmail.com>
2014-12-20 17:48:37 -0800

    Unused service

--- app/services/collapseService.js
+++
@@ -1,8 +0,0 @@
-// Collapse function for mobile view
-app.factory('Collapse', function($window) {
-    var isCollapsed = true;
-    return {
-        getState: function() { return isCollapsed; },
-        flipState: function() { if($window.innerWidth < 768) isCollapsed = !isCollapsed; }
-    };
-});
