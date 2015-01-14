48180d6c8017be2cfaee85bbe3ff08c385e1b2a8
Takeshi Mochida <takeshimochida@gmail.com>
2015-01-14 14:45:49 -0500

    configure Ripple wallet address in setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,14 +2,14 @@
 #
 # Configures database connection and hot wallet address (used by BillSplit).
 
+echo "Please make sure you are running 'scripts/setup.sh' from the directory. If your working directory is scripts, please cd to parent directory."
+echo -ne "===============================\n"
 echo "===== Setting up database ====="
+echo "==============================="
 
 read -p "Database host [localhost]: " dbhost
-
 read -p "Database name [billsplit]: " dbname
-
 read -p "Database user [billsplit]: " dbuser
-
 stty -echo
 read -p "Database password [ripple]: " dbpass
 stty echo
@@ -19,21 +19,28 @@ dbname=${dbname:-billsplit}
 dbuser=${dbuser:-billsplit}
 dbpass=${dbpass:-ripple}
 
+echo -ne "\n\n=====================================\n"
+echo "===== Configuring Ripple wallet ====="
+echo "====================================="
 
-echo -ne "\n===== Configuring Ripple wallet ====="
+read -p "Hot wallet address [null]: " wallet_addr
 
-read -p "Hot wallet address: " wallet_addr
+wallet_addr=${wallet_addr:-null}
 
-echo -ne "\n"
+sed -i "s/\$dbhost=.*/\$dbhost=\"$dbhost\"/g" api/config.php
+sed -i "s/\$dbname=.*/\$dbname=\"$dbname\"/g" api/config.php
+sed -i "s/\$dbuser=.*/\$dbuser=\"$dbuser\"/g" api/config.php
+sed -i "s/\$dbpass=.*/\$dbpass=\"$dbpass\"/g" api/config.php
+sed -i "s/\$hot_wallet=.*/\$hot_wallet=\"$wallet_addr\"/g" api/config.php
 
+echo -ne "\n\n==================================\n"
+echo "===== Configuration summary ======"
+echo "=================================="
+
+#Configured database and wallet as follows:\n"
 echo "DB host: $dbhost"
 echo "DB name: $dbname"
 echo "DB user: $dbuser"
 echo "DB pass: $dbpass"
 echo "Wallet address: $wallet_addr"
 
-
-sed -i "s/\$dbhost=.*/\$dbhost=\"$dbhost\"/g" api/dbconfig.php 
-sed -i "s/\$dbname=.*/\$dbname=\"$dbname\"/g" api/dbconfig.php 
-sed -i "s/\$dbuser=.*/\$dbuser=\"$dbuser\"/g" api/dbconfig.php 
-sed -i "s/\$dbpass=.*/\$dbpass=\"$dbpass\"/g" api/dbconfig.php 
